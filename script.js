cdcdaa1942c5994b0ec1ab62038d28c5b606d9db
DZZT2004 <taodaiuwu@gmail.com>
2022-11-03 09:59:17 +0100

    dadi finito

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// palindromo
+
 function palindroma(p){
     let inverso = "";
     let contatore = p.length - 1;
@@ -14,3 +16,30 @@ function palindroma(p){
 
 var parola = prompt("Inserisci una parola ");
 palindroma(parola);
+
+// dadi
+
+function pariDispari(num){
+    if(num%2 == 0){
+        return "pari";
+    }else{
+        return "dispari";
+    }
+}
+
+var pariOdispari = prompt("Scegli tra pari e dispari");
+var numeroUtente = prompt("Inserisci un numero da 1 a 5 ");
+var numeroComputer = Math.floor(Math.random() * 5) + 1;
+var somma = numeroUtente + numeroComputer;
+var risultato;
+if(pariDispari(somma) == "pari"){
+    risultato = "pari";
+}else if(pariDispari(somma) == "dispari"){
+    risultato = "dispari";
+}
+
+if(pariOdispari == risultato){
+    console.log("Il giocatore ha vinto");
+}else{
+    console.log("Il computer ha vinto");
+}
